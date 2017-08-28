a50969a8f18c44d5f5a2968122429c3cc04fdcd9
giganicola <giganicola@gmail.com>
2017-08-28 10:19:57 -0400

    Navigation update

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -9,6 +9,16 @@ use App\Post;
 
 class BlogController extends Controller
 {
+
+     public function getIndex() {
+    
+        //fetch from the DB based on slug
+        $posts = Post::paginate(10);
+        
+        // return the view and pass in the post object
+        return view('blog.index')->withPosts($posts);
+    }
+  
     public function getSingle($slug) {
     
         //fetch from the DB based on slug
--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
   <div class="row">
-      <div class="ol-md-8 col-md-offset-2">
+      <div class="col-md-8 col-md-offset-2">
           <h1>{{ $post->title }}</h1>
           <p>{{ $post->body }}</p>
       </div>
