410d248e60890b5995d495966e85aaef8d910912
giganicola <giganicola@gmail.com>
2017-09-07 11:48:45 -0400

    Tags CRUD

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -54,7 +54,8 @@ class TagController extends Controller
      */
     public function show($id)
     {
-        //
+        $tag = Tag::find($id);
+        return view('tags.show')->withTag($tag);
     }
 
     /**
