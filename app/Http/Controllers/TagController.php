14f0a3efde20c63fbe2493ca1adc3706ae0eb797
giganicola <giganicola@gmail.com>
2017-09-08 05:05:19 -0400

    Tags edit

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -66,7 +66,8 @@ class TagController extends Controller
      */
     public function edit($id)
     {
-        //
+        $tag = Tag::find($id);
+        return view('tags.edit')->withTag($tag);
     }
 
     /**
@@ -78,7 +79,15 @@ class TagController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $tag = Tag::find($id);
+      
+        $this->validate($request, ['name' => 'required|max:255']);
+      
+        $tag->name = $request->name;
+        $tag->save();
+      
+        Session::flash('success', 'Succesfully saved your tag');
+        return redirect()->route('tags.show', $tag->id);
     }
 
     /**
