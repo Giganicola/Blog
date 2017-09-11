249bc042a6ad8473f6bcf1b8cddee83527973d88
giganicola <giganicola@gmail.com>
2017-09-11 04:58:07 -0400

    Completed contact form

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,37 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 use App\Post;
+use Mail;
+use Session;
 
 class PagesController extends Controller {
   
     public function getIndex() {
-        
         $posts = Post::orderBy('created_at', 'desc')->limit(4)->get();
-      
         return view('pages.welcome')->withPosts($posts);
     }
   
     public function getAbout() {        
-        
         $first = '';
         $last = 'Blog';
-      
         $data = [];
-      
         $data['fullname'] = $first . ' ' . $last;
         $data['email'] = 'test@test.com';
       
         return view('pages.about')->withData($data);
     }
   
-    public function getContact() {
-      
+    public function getContact() {      
         return view('pages.contact');
     }  
   
+    public function postContact(Request $request) {      
+        $this->validate($request, [
+          'email' => 'required|email',
+          'subject' => 'min:3',
+          'message' => 'min:10']);
+        
+          $data = array(
+              'email' => $request->email,
+              'subject' => $request->subject,
+              'bodyMessage' => $request->message
+          );
+      
+          Mail::send('emails.contact', $data, function($message) use ($data) {
+              $message->from($data['email']);
+              $message->to('test@test.com');
+              $message->subject($data['subject']);
+          });     
+      
+          Session::flash('success', 'Your email was Sent!');
+           
+          return redirect()->route('contact');
+    }  
+  
+  
+  
 }
-
-
-
 ?>
--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -6,7 +6,8 @@
     <div class="row">
         <div class="col-md-12">
             <h1>Contact</h1>
-            <form>
+            <form action="{{ url('contact') }}" method="POST">
+                {{ csrf_field() }}
                 <div class="form-group">
                     <label name="email">Email:</label>
                     <input id="email" name="email" class="form-control">
